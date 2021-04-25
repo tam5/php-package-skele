59aca5376d94c3f961d25b60be57eafa1a8f400a
tam5 <arimiller92@gmail.com>
2021-04-25 11:56:48 -0400

    fail a linter

--- src/Example.php
+++ src/Example.php
@@ -4,6 +4,9 @@ namespace LiveIntent;
 
 class Example
 {
+    public function badLinter() {
+         $this->fail;
+    }
     // Make a change here, and here
     // another thing
     // change more stuffs

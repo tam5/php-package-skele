2187439546816c8be46cb3b1e288242c923dd10e
tam5 <arimiller92@gmail.com>
2021-04-25 16:11:07 -0400

    more changes

--- src/Example.php
+++ src/Example.php
@@ -2,6 +2,23 @@
 namespace LiveIntent;
 
 class Example {
+    /**
+     * @return bool
+     */
+    public function firstMethod()
+    {
+        return 123;
+    }
+
+    /**
+     *
+     */
+    public function secondMethod( ) {
+        echo 'this is not psr2';
+
+    }
+
+
     // Make a change here, and here
     // another thing
     // change more stuffs

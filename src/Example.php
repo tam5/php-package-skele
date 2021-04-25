91b9916d05a9014ca033175133dd9374b89dc2b0
tam5 <arimiller92@gmail.com>
2021-04-25 10:36:16 -0400

    a thing

--- src/Example.php
+++ src/Example.php
@@ -4,6 +4,17 @@ namespace LiveIntent;
 
 class Example
 {
+    /**
+     * @return bool
+     */
+    public function psalmFail()
+    {
+        return 'foo';
+    }
+
+    public function lintFail() { 
+        return 'hihi' ;
+    }
     // Make a change here, and here
     // another thing
     // change more stuffs
